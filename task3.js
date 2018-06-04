d51c661477eaba94e776110327e9a183da66c87e
zubal96 <zubal96@mail.ru>
2018-06-04 16:22:42 +0500

    Update task3.js

--- task3.js
+++ task3.js
@@ -15,24 +15,28 @@ switch(hours){
 }
 
 if(minutes!==0){
-	if(str!="") str+=" ";
+	if(str!="") 
+		str+=" ";
+	if(minutes >= 11 && minutes <= 14)
+		str+=minutes+" минут";
+	else
 	switch(minutes%10){
 		case 1: str+=minutes+" минута"; break; 
 		case 2: case 3: case 4: str+= minutes+" минуты"; break; 
 		default: str+=minutes+" минут";
 	}
-	if(minutes >= 11 && minutes <= 14)
-		str+=minutes+" минут";
 }
 
 if(seconds!==0){
-	if(str!="") str+=" ";
+	if(str!="") 
+		str+=" ";
+	if(seconds >= 11 && seconds <= 14)
+		str+=seconds+" секунд";
+	else
 	switch(seconds%10){
 		case 1: str+=seconds+" секунда"; break; 
 		case 2: case 3: case 4: str+=seconds+" секунды"; break; 
 		default: str+=seconds+" секунд";
 	}
-	if(seconds >= 11 && seconds <= 14)
-		str+=seconds+" секунд";
 }
 console.log(str);
