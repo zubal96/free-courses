bc19f64f1bf69e470552a5f84f7cc727ee718d04
zubal96 <zubal96@mail.ru>
2018-06-04 19:49:15 +0500

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,42 +1,43 @@
 /* Task 3 */
-let t1 = process.argv[2];
-let t2 = process.argv[3];
-let time = new Date(0,0,0,0,0,+t1 + +t2);
-let hours = time.getHours();
-let minutes = time.getMinutes();
-let seconds = time.getSeconds();
-let str ="";
+function sumTime(t1,t2){
+	var time = new Date(0,0,0,0,0,+t1 + +t2);
+	var hours = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
+	var str ="";
 
-switch(hours){
-	case 0: break; 
-	case 1: str+=hours+" час"; break; 
-	case 2: case 3: case 4: str+=hours+" часа"; break; 
-	default: str+=hours+" часов";
-}
-
-if(minutes!==0){
-	if(str!="") 
-		str+=" ";
-	if(minutes >= 11 && minutes <= 14)
-		str+=minutes+" минут";
-	else
-	switch(minutes%10){
-		case 1: str+=minutes+" минута"; break; 
-		case 2: case 3: case 4: str+= minutes+" минуты"; break; 
-		default: str+=minutes+" минут";
+	switch(hours){
+		case 0: break; 
+		case 1: str+=hours+" час"; break; 
+		case 2: case 3: case 4: str+=hours+" часа"; break; 
+		default: str+=hours+" часов";
 	}
-}
 
-if(seconds!==0){
-	if(str!="") 
-		str+=" ";
-	if(seconds >= 11 && seconds <= 14)
-		str+=seconds+" секунд";
-	else
-	switch(seconds%10){
-		case 1: str+=seconds+" секунда"; break; 
-		case 2: case 3: case 4: str+=seconds+" секунды"; break; 
-		default: str+=seconds+" секунд";
+	if(minutes!==0){
+		if(str!="") 
+			str+=" ";
+		if(minutes >= 11 && minutes <= 14)
+			str+=minutes+" минут";
+		else
+			switch(minutes%10){
+				case 1: str+=minutes+" минута"; break; 
+				case 2: case 3: case 4: str+= minutes+" минуты"; break; 
+				default: str+=minutes+" минут";
+			}
 	}
-}
-console.log(str);
+
+	if(seconds!==0){
+		if(str!="") 
+			str+=" ";
+		if(seconds >= 11 && seconds <= 14)
+			str+=seconds+" секунд";
+		else
+			switch(seconds%10){
+				case 1: str+=seconds+" секунда"; break; 
+				case 2: case 3: case 4: str+=seconds+" секунды"; break; 
+				default: str+=seconds+" секунд";
+			}
+	}
+	return str;
+}	
+process.stdout.write(sumTime(process.argv[2],process.argv[3]));
